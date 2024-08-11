ff2f5530dea6a95ac538b60df6517dc1c21fa812
William Kim <william.kim@xplortechnologies.com>
2024-08-10 18:49:57 -0700

    add code for Second push and pull testing

--- indexJav.js
+++ indexJav.js
@@ -5,8 +5,8 @@ function loginCheck(){
     let userName = document.getElementById("userName").value;
     let password = document.getElementById("password").value;
     
-    let userNames = ["James","Aiden", "Ethan", "Nathan", "Jaiden", "William"];
-    let passwords = ["123","234","345","456","678", "999"];
+    let userNames = ["James","Aiden", "Ethan", "Nathan", "Jaiden", "William", "Jamez"];
+    let passwords = ["123","234","345","456","678", "999", "888"];
 
     let isMatched = false;
 
